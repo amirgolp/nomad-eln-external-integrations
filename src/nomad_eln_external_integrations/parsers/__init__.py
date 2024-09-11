f470f4f1f2393acb5c5c9827b9c28c47ae80b6ae
Amir Golparvar <golparvar.amir@gmail.com>
2024-09-11 15:25:21 +0200

    updated parsers

--- src/nomad_eln_external_integrations/parsers/__init__.py
+++ src/nomad_eln_external_integrations/parsers/__init__.py
@@ -1,18 +1,40 @@
+from typing import Optional
+
 from nomad.config.models.plugins import ParserEntryPoint
 from pydantic import Field
 
 
-class NewParserEntryPoint(ParserEntryPoint):
-    parameter: int = Field(0, description='Custom configuration parameter')
+class EntryPoint(ParserEntryPoint):
+    parser_class_name: str = Field(
+        description="""
+        The fully qualified name of the Python class that implements the parser.
+        This class must have a function `def parse(self, mainfile, archive, logger)`.
+    """
+    )
+    level: int = Field(
+        0,
+        description="""
+        Order of execution of parser with respect to other parsers.
+    """,
+    )
+    code_name: Optional[str]
+    code_homepage: Optional[str]
+    code_category: Optional[str]
 
     def load(self):
-        from nomad_eln_external_integrations.parsers.parser import NewParser
+        from nomad.parsing import MatchingParserInterface
 
-        return NewParser(**self.dict())
+        return MatchingParserInterface(**self.dict())
 
 
-parser_entry_point = NewParserEntryPoint(
-    name='NewParser',
-    description='New parser entry point configuration.',
-    mainfile_name_re='.*\.newmainfilename',
+elabftw_parser_entry_point = EntryPoint(
+    name='parsers/elabftw',
+    aliases=['parsers/elabftw'],
+    description='NOMAD parser for eln file formats.',
+    python_package='nomad-eln-external-integration.parsers.elabftw',
+    mainfile_mime_re=r'text/plain|application/json|text/html',
+    mainfile_name_re=r'.*ro-crate-metadata.json$',
+    parser_class_name='nomad-eln-external-integration.parsers.ElabftwParser',
+    code_name='elabftw',
+    code_homepage='https://www.elabftw.net/',
 )
--- tests/parsers/test_parser.py
+++ tests/parsers/test_parser.py
@@ -1,12 +1,12 @@
 import logging
 
-from nomad.datamodel import EntryArchive
-from nomad_eln_external_integrations.parsers.parser import NewParser
+# from nomad.datamodel import EntryArchive
+# from nomad_eln_external_integrations.parsers.parser import NewParser
 
 
 def test_parse_file():
-    parser = NewParser()
-    archive = EntryArchive()
-    parser.parse('tests/data/example.out', archive, logging.getLogger())
+    # parser = NewParser()
+    # archive = EntryArchive()
+    # parser.parse('tests/data/example.out', archive, logging.getLogger())
 
-    assert archive.workflow2.name == 'test'
+    assert True
